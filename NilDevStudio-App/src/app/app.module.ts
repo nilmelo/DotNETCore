1c9786db531e17a520d128d2cd59489e2ab06196
NilDevStudio <nildevstudio@outlook.com>
2020-09-23 14:36:19 -0300

    Angular app

---
+++ NilDevStudio-App/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EventsComponent } from './events/events.component';
+
+@NgModule({
+  declarations: [	
+    AppComponent,
+      EventsComponent
+   ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
